ce8c055e9350c0247432d74b079b0ea736397e24
Mats Egnell <mattegol.egnell@gmail.com>
2018-09-12 13:16:25 +0200

    Add styling with Bootswatch Superhero theme.

--- YellowShop/App_Start/BundleConfig.cs
+++ YellowShop/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace YellowShop
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-superhero.css",
                       "~/Content/site.css"));
         }
     }
